9047ad148af0c3c249d072cefa84ada348d22d1c
Illya Bomash <ibomash@knewton.com>
2013-11-16 15:05:48 -0500

    Smarter date parsing in Pandas conversation loading

--- munging/ctl_utils.py
+++ munging/ctl_utils.py
@@ -7,26 +7,33 @@ pip install sklearn
 
 """
 
+import crisis.parsers
 
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
 import locale
 import numpy as np
+from datetime import datetime, timedelta
 
 def parse_messages(path='dk_message_level_131114.csv'):
     data = pd.read_csv(path,
             parse_dates= [7,8,9, 23, 4],
             converters={ 'm_id' : convert_to_int, 'c_id' : convert_to_int,
-                'specialist_id' : convert_to_int },
+                         'specialist_id' : convert_to_int },
             skiprows=[1],
             )
     return data
 
 def parse_conversations(path='dk_conversation_level_1311114.csv'):
     data = pd.read_csv(path,
-            parse_dates= [7,8,9],
+            #parse_dates= [7,8,9],
             converters={ 'm_id' : convert_to_int, 'c_id' : convert_to_int,
-                'specialist_id' : convert_to_int },
+                         'specialist_id' : convert_to_int,
+                         'conv_start': convert_to_date,
+                         'conv_end': convert_to_date,
+                         'first_msg': convert_to_date,
+                         'last_msg': convert_to_date,
+                         'addedtoqueue': convert_to_date,
+                         'takenfromqueue': convert_to_date},
             skiprows=[1],
             )
     return data
@@ -37,11 +44,22 @@ def convert_to_int(x):
 	except:
 		return np.nan
 
+
+def convert_to_date(x):
+    """Converter for dates.
+    """
+    x = crisis.parsers.caster(x)
+    if not isinstance(x, datetime):
+        x = np.nan
+    return x
+
+
 """
 Adds binary flag for each issue
 
 """
 def add_issue_columns(messages):
+	from sklearn.feature_extraction.text import CountVectorizer
 	v = CountVectorizer(binary=True)
 	issue_matrix = v.fit_transform([str(x) for x in messages['Q13_issues']]).toarray()
 	issues = v.get_feature_names()
