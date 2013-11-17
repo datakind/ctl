3499c694884e073df7d56278579e522c1a24a8bf
dlovell <dlovell@gmail.com>
2013-11-17 11:05:15 -0500

    annotate plots with {x,y} labels, titles; save plots

--- analysis/sdl_analysis.py
+++ analysis/sdl_analysis.py
@@ -17,7 +17,9 @@ conversation_filename = os.path.join(C.this_repo_dir, C.CONVERSATION_DATA_FILENA
 conversations = fu.process_or_read_hdf(conversation_filename, cu.parse_conversations, 'conversations')
 message_filename = os.path.join(C.this_repo_dir, C.MESSAGE_DATA_FILENAME)
 messages = fu.process_or_read_hdf(message_filename, cu.parse_messages, 'messages')
-#
+
+
+# subset the data
 conversation_ids = conversations.c_id.values
 #
 teen_messages = su.filter_dataframe_rows(messages, su.is_teen)
@@ -33,22 +35,28 @@ counselor_unrated_messages = su.filter_dataframe_rows(counselor_messages, su.is_
 
 
 # verify message, conversation ids increment sanely
-pylab.figure()
+fh = pylab.figure()
 messages.c_id.plot()
+pylab.xlabel('message index')
+pylab.ylabel('conversation id')
+pylab.title('conversation id is monotonically increasing\nrows must be stored in conversation order')
+pylab.savefig('conversation_ids')
 #
 pylab.figure()
 messages.m_id.map(numpy.float).plot()
+pylab.xlabel('message index')
+pylab.ylabel('message id')
+pylab.title('message ids not monotonic')
+pylab.savefig('message_ids')
 #
 pylab.figure()
 messages.m_id.map(numpy.float).plot(linestyle='', marker='x', markersize=3)
 pylab.xlim(42000, 45000)
 pylab.ylim(45000, 48001)
-#
-pylab.figure()
-is_teen = messages.actor_type == 'MobileMessenger'
-messages.m_id[is_teen].map(numpy.float).plot(linestyle='', marker='x', markersize=3)
-pylab.xlim(42000, 45000)
-pylab.ylim(45000, 48001)
+pylab.xlabel('message index')
+pylab.ylabel('message id')
+pylab.title('zoom in to get a sense for conversation overlaps')
+pylab.savefig('conversation_overlap')
 
 
 # generate top level message lengths
@@ -92,6 +100,7 @@ t_fh = pylab.title('teen message lengths')
 ax3 = pylab.subplot(gs[1,1])
 bp_fh = ax3.boxplot(counselor_conversation_message_lengths)
 t_fh = pylab.title('counselor message lengths')
+pylab.savefig('all_teen_counselor_conversation_message_length_boxplots')
 
 # teen {good, bad} conversations
 pylab.figure()
@@ -116,6 +125,7 @@ ax3 = pylab.subplot(gs[1, 1])
 bp_fh = ax3.boxplot(teen_bad_conversation_message_lengths)
 ax3.set_ylim((0, 300))
 t_fh = pylab.title('teen bad message lengths')
+pylab.savefig('teen_unrated_good_bad_message_length_boxplots')
 
 # counselor {good, bad} conversations
 pylab.figure()
@@ -140,4 +150,5 @@ ax3 = pylab.subplot(gs[1, 1])
 bp_fh = ax3.boxplot(counselor_bad_conversation_message_lengths)
 ax3.set_ylim((0, 300))
 t_fh = pylab.title('counselor bad message lengths')
+pylab.savefig('counselor_unrated_good_bad_message_length_boxplots')
 
