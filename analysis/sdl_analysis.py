8b39e439656b31d73c3671f9bc1b0f2410513d6d
dlovell <dlovell@gmail.com>
2013-11-16 22:37:35 -0500

    create separate plots for unrated conversations

--- analysis/sdl_analysis.py
+++ analysis/sdl_analysis.py
@@ -28,9 +28,11 @@ counselor_messages = su.filter_dataframe_rows(messages, su.is_counselor)
 #
 teen_good_messages = su.filter_dataframe_rows(teen_messages, su.is_good)
 teen_bad_messages = su.filter_dataframe_rows(teen_messages, su.is_bad)
+teen_unrated_messages = su.filter_dataframe_rows(teen_messages, su.is_unrated)
 #
 counselor_good_messages = su.filter_dataframe_rows(counselor_messages, su.is_good)
 counselor_bad_messages = su.filter_dataframe_rows(counselor_messages, su.is_bad)
+counselor_unrated_messages = su.filter_dataframe_rows(counselor_messages, su.is_unrated)
 
 
 # verify message, conversation ids increment sanely
@@ -65,66 +67,80 @@ teen_good_conversation_message_lengths = su.generate_conversation_message_length
         teen_good_messages, conversation_ids, truncate_conversation_to)
 teen_bad_conversation_message_lengths = su.generate_conversation_message_lengths(
         teen_bad_messages, conversation_ids, truncate_conversation_to)
+teen_unrated_conversation_message_lengths = su.generate_conversation_message_lengths(
+        teen_unrated_messages, conversation_ids, truncate_conversation_to)
 # generate counselor level message lengths
 counselor_good_conversation_message_lengths = su.generate_conversation_message_lengths(
         counselor_good_messages, conversation_ids, truncate_conversation_to)
 counselor_bad_conversation_message_lengths = su.generate_conversation_message_lengths(
         counselor_bad_messages, conversation_ids, truncate_conversation_to)
+counselor_unrated_conversation_message_lengths = su.generate_conversation_message_lengths(
+        counselor_unrated_messages, conversation_ids, truncate_conversation_to)
 
 
 # plot some conversation metrics
 
 # {all, teen, counselor} conversations
 pylab.figure()
-gs = gridspec.GridSpec(2,4)
+gs = gridspec.GridSpec(2,2)
 #
-ax1 = pylab.subplot(gs[0, 1:-1])
-ax1.boxplot(conversation_message_lengths)
-pylab.title('all message lengths')
+ax0 = pylab.subplot(gs[0, 0])
+bp_fh = ax0.boxplot(conversation_message_lengths)
+t_fh = pylab.title('all message lengths')
 #
-ax2 = pylab.subplot(gs[1,:2])
-ax2.boxplot(teen_conversation_message_lengths)
-pylab.title('teen message lengths')
+ax2 = pylab.subplot(gs[1,0])
+bp_fh = ax2.boxplot(teen_conversation_message_lengths)
+t_fh = pylab.title('teen message lengths')
 #
-ax3 = pylab.subplot(gs[1,2:])
-ax3.boxplot(counselor_conversation_message_lengths)
-pylab.title('counselor message lengths')
+ax3 = pylab.subplot(gs[1,1])
+bp_fh = ax3.boxplot(counselor_conversation_message_lengths)
+t_fh = pylab.title('counselor message lengths')
 
 # teen {good, bad} conversations
 pylab.figure()
-gs = gridspec.GridSpec(2,4)
+gs = gridspec.GridSpec(2,2)
 #
-ax1 = pylab.subplot(gs[0, 1:-1])
-ax1.boxplot(teen_conversation_message_lengths)
+ax0 = pylab.subplot(gs[0, 0])
+bp_fh = ax0.boxplot(teen_conversation_message_lengths)
+ax0.set_ylim((0, 300))
+t_fh = pylab.title('teen message lengths')
+#
+ax1 = pylab.subplot(gs[0, 1])
+bp_fh = ax1.boxplot(teen_unrated_conversation_message_lengths)
 ax1.set_ylim((0, 300))
-pylab.title('teen message lengths')
+t_fh = pylab.title('teen unrated message lengths')
 #
-ax2 = pylab.subplot(gs[1,:2])
-ax2.boxplot(teen_good_conversation_message_lengths)
+ax2 = pylab.subplot(gs[1, 0])
+bp_fh = ax2.boxplot(teen_good_conversation_message_lengths)
 ax2.set_ylim((0, 300))
 pylab.title('teen good message lengths')
 #
-ax3 = pylab.subplot(gs[1,2:])
-ax3.boxplot(teen_bad_conversation_message_lengths)
+ax3 = pylab.subplot(gs[1, 1])
+bp_fh = ax3.boxplot(teen_bad_conversation_message_lengths)
 ax3.set_ylim((0, 300))
-pylab.title('teen bad message lengths')
+t_fh = pylab.title('teen bad message lengths')
 
 # counselor {good, bad} conversations
 pylab.figure()
-gs = gridspec.GridSpec(2,4)
+gs = gridspec.GridSpec(2,2)
 #
-ax1 = pylab.subplot(gs[0, 1:-1])
-ax1.boxplot(counselor_conversation_message_lengths)
+ax0 = pylab.subplot(gs[0, 0])
+bp_fh = ax0.boxplot(counselor_conversation_message_lengths)
+ax0.set_ylim((0, 300))
+t_fh = pylab.title('counselor message lengths')
+#
+ax1 = pylab.subplot(gs[0, 1])
+bp_fh = ax1.boxplot(counselor_unrated_conversation_message_lengths)
 ax1.set_ylim((0, 300))
-pylab.title('counselor message lengths')
+t_fh = pylab.title('counselor unrated message lengths')
 #
-ax2 = pylab.subplot(gs[1,:2])
-ax2.boxplot(counselor_good_conversation_message_lengths)
+ax2 = pylab.subplot(gs[1, 0])
+bp_fh = ax2.boxplot(counselor_good_conversation_message_lengths)
 ax2.set_ylim((0, 300))
-pylab.title('counselor good message lengths')
+t_fh = pylab.title('counselor good message lengths')
 #
-ax3 = pylab.subplot(gs[1,2:])
-ax3.boxplot(counselor_bad_conversation_message_lengths)
+ax3 = pylab.subplot(gs[1, 1])
+bp_fh = ax3.boxplot(counselor_bad_conversation_message_lengths)
 ax3.set_ylim((0, 300))
-pylab.title('counselor bad message lengths')
+t_fh = pylab.title('counselor bad message lengths')
 
